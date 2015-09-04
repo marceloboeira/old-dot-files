767dc80af42a0f432a9c302f76a6f4ceb5f5d6f6
Marcelo Boeira <me@marceloboeira.com>
2015-09-04 19:52:35 -0300

    refactor: Move alias to a specific file

--- fish/config.fish
+++ fish/config.fish
@@ -7,12 +7,3 @@ set -x PATH /usr/local/bin /usr/local/sbin $PATH
 rvm current > /dev/null
 rvm > /dev/null
 
-alias c=clear
-alias g=git 
-alias gs="git s"
-alias gc="git cm"
-alias b=bundle
-alias be="bundle exec"
-alias bu="bundle update"
-alias bi="bundle install"
-alias v="mvim"
---
+++ fish/functions/alias.fish
@@ -0,0 +1,10 @@
+alias c=clear
+alias g=git 
+alias gs="git s"
+alias gc="git cm"
+alias b=bundle
+alias be="bundle exec"
+alias bu="bundle update"
+alias bi="bundle install"
+alias v="mvim"
+
